1b9bfad21afa0c99d416d078a6b1d5201a45726c
Diangis <nissan200oe@yandex.ru>
2024-01-09 15:51:51 +0300

    FirstCommit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router/index';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
