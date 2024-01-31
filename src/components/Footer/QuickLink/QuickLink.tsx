3e466ffa3d88f7db222fe62423bc847b7954e801
Diangis <nissan200oe@yandex.ru>
2024-01-31 10:04:10 +0300

    Added some interactive

--- src/components/Footer/QuickLink/QuickLink.tsx
+++ src/components/Footer/QuickLink/QuickLink.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from '../Footer.modules.css';
+import style from './QuickLink.modules.css';
 
 export default function QuickLink() {
   return (
@@ -7,32 +8,32 @@ export default function QuickLink() {
       <h3 className={styles.title}>Quick Link</h3>
       <ul className={styles.list}>
         <li>
-          <Link to="/" className={styles.text}>
+          <Link to="/" className={`${styles.text} ${style.link}`}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="/blog" className={styles.text}>
+          <Link to="/blog" className={`${styles.text} ${style.link}`}>
             About
           </Link>
         </li>
         <li>
-          <Link to="/singlepost" className={styles.text}>
+          <Link to="/singlepost" className={`${styles.text} ${style.link}`}>
             Blog
           </Link>
         </li>
         <li>
-          <Link to="/" className={styles.text}>
+          <Link to="/" className={`${styles.text} ${style.link}`}>
             Archived
           </Link>
         </li>
         <li>
-          <Link to="/author" className={styles.text}>
+          <Link to="/author" className={`${styles.text} ${style.link}`}>
             Author
           </Link>
         </li>
         <li>
-          <Link to="/" className={styles.text}>
+          <Link to="/" className={`${styles.text} ${style.link}`}>
             Contact
           </Link>
         </li>
