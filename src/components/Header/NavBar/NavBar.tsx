38fe3e8eb1cb2041c034af5c5b7069bd7290045d
Diangis <nissan200oe@yandex.ru>
2024-02-05 11:43:49 +0300

    Text Component added (alpha)

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './NavBar.modules.css';
 import { PATH_LIST } from 'constants/index';
+import Text from 'components/common/Text/Text';
 
 export function NavBar() {
   return (
     <nav>
       <ul className={styles.list}>
         <NavLink className={`${styles.li} ${styles.a}`} to={PATH_LIST.ROOT}>
-          Home
+          <Text content="Home" color="black" bold="400" />
         </NavLink>
         <NavLink className={`${styles.li} ${styles.a}`} to={PATH_LIST.BLOG}>
           Blog
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,19 +1,18 @@
-export const ROOT = '/';
-export const BLOG = 'blog';
-export const POST = 'post';
-export const AUTHOR = 'author';
+// export const ROOT = '/';
+// export const BLOG = 'blog';
+// export const POST = 'post';
+// export const AUTHOR = 'author';
 
-export const PATH_LIST = {
-  ROOT,
-  BLOG: `${ROOT}${BLOG}`,
-  POST: `${ROOT}${POST}`,
-  AUTHOR: `${ROOT}${AUTHOR}`,
-};
+// export const PATH_LIST = {
+//   ROOT,
+//   BLOG: `${ROOT}${BLOG}`,
+//   POST: `${ROOT}${POST}`,
+//   AUTHOR: `${ROOT}${AUTHOR}`,
+// };
 
-// export enum PathList {
-//   ROOT = '/',
-//   EDIT = 'edit',
-//   TASK_ID = 'taskId',
-//   ADD = 'add',
-// }
-// PathList.ROOT
+export enum PATH_LIST {
+  ROOT = '/',
+  BLOG = 'blog',
+  POST = 'post',
+  AUTHOR = 'author',
+}
